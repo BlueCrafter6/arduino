c4a6cdfd1339f4e1d9c4ef9a7e0c92e80af85ba4
Blue Crafter <87858714+BlueCrafter6@users.noreply.github.com>
2021-09-29 20:55:32 +0200

    ➕ Add Project Code

---
+++ Project 5/RGB_Light/RGB_Light.ino
@@ -0,0 +1,21 @@
+int redPin = 8;
+int greenPin = 5;
+int bluePin = 13;
+
+void setup() {
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+
+}
+
+void loop() {
+  colorRGB(random(0,255), random(0,255), random(0,255));
+  delay(1000);
+}
+
+void colorRGB(int red, int green, int blue) {
+  analogWrite(redPin,constrain(red,0,255));
+  analogWrite(greenPin,constrain(green,0,255));
+  analogWrite(bluePin,constrain(blue,0,255));
+}
